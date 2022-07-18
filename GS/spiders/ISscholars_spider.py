18108090c76930fc4a3ffa9549a41197a3179d90
Sina <106835733+SinaStB@users.noreply.github.com>
2022-07-18 11:40:01 -0500

    Update ISscholars_spider.py

--- GS/spiders/ISscholars_spider.py
+++ GS/spiders/ISscholars_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
 import csv
 import os
 import datetime, pytz
@@ -10,10 +9,8 @@ logging.disable('WARNING')
 
 class scholarspider(scrapy.Spider):
     name = 'sch'
-    # sch_names = []
-    # start = 0
 
-    #these urls will return html files with Information Systems scholars from Google Scholar website
+    #this url will return html files (first page) with Information Systems scholars from Google Scholar website
     start_urls = ['https://scholar.google.com/citations?view_op=search_authors&hl=en&mauthors=label:information_systems',]
 
     def parse(self, response):
@@ -22,16 +19,14 @@ class scholarspider(scrapy.Spider):
         now = pytz.timezone("America/Chicago").localize(datetime.datetime.now())
         file_name = "IS scholars_GS" + now.strftime("%Y-%m-%d") + ".csv"
 
-        #making the output file (if it does'nt exist already), naming it and writting the header row in it
+        #making the output file (if it doesn't exist already), naming it and writting the header row in it
         if os.path.exists(file_name):
-            out_file = open(file_name, 'a')
+            out_file = open(file_name, 'a', newline='', encoding="utf-8", errors='ignore')
             csv_writer = csv.writer(out_file)
         else:
             out_file = open(file_name, 'w')
-            csv_writer = csv.writer(out_file)
+            csv_writer = csv.writer(out_file, newline='')
             csv_writer.writerow(['name','GS_url','affiliation','citation_count','interests','interests_url'])
-
-        # repeating = False
 
         #requesting HTML elements to fill the csv
         for scholar in response.css("div.gsc_1usr"):
@@ -43,32 +38,16 @@ class scholarspider(scrapy.Spider):
             int_url = scholar.css('div.gs_ai_int a::attr(href)').getall()
             csv_writer.writerow([sch_name,sch_url,aff,cby,int,int_url])
 
+        #getting url from next button and turn it into usable url
         next_page = response.url.split('?')[0] + '?' + response.css('.gs_btnPR::attr(onclick)').get().split('?')[1]
-        # next_path = response.css('.gs_btnPR::attr(onclick)').get()
         next_page = next_page.replace("'", "")
         next_page = codecs.decode(next_page, 'unicode-escape')
-        print(next_page)
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
 
-        #checks for duplicate names
-        # if sch_name not in self.sch_names:
-        #     csv_writer.writerow([sch_name,sch_url,aff,cby,int,int_url])
-        #     self.sch_names.append(sch_name)
-        #     self.start += 1
-        # else:
-        #     repeating = True
-
-        #checks for pagination
-        # if len(scholars) >= 10 and not repeating:
-        #     url = response.url.split('&start=')[0] + "&start=" + str(self.start)
-        #     yield scrapy.Request(
-        #         url = url,
-        #         callback = self.parse
-        #     )
-        # else:
-        #     print('No page left')
+        if next_page is None:
+            print('No page left')
+        else:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
         #closing the opened file
         out_file.close()
